425e53ed9a8d1d9bb72680ab5fcbc81f66e85e79
developer-shkim <developer.shkim.gmail.com>
2021-07-02 23:56:46 +0900

    feat : 환경설정, 웰컴 페이지 구현

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "test!!");
+        return "hello"; // resources/templates/"hello" 찾아서 렌더링
+    }
+}
