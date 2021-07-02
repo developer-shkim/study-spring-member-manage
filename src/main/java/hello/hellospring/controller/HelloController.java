099a56cc4951fad7de377597d925d17f0c5717f2
developer-shkim <developer.shkim.gmail.com>
2021-07-03 08:37:48 +0900

    feat : 정적 컨텐츠, MVC 방식으로 웰컴 페이지 구현

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,23 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "test!!");
+        model.addAttribute("data", "hello!!");
         return "hello"; // resources/templates/"hello" 찾아서 렌더링
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    // 단축키: 커맨드 + P -> 파라미터 정보 볼 수 있다.
+    // required true 이면 기본으로 값을 넘겨야 한다.
+    // http://localhost:8080/hello-mvc?name=spring!!!!!! -> hello spring!!!!!! 출력
 }
