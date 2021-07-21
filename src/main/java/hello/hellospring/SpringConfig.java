1cf73fcf804ab78a0e82c17ffe29ea3fa9649ec5
developer-shkim <developer.shkim@gmail.com>
2021-07-21 22:09:26 +0900

    feat : 스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -16,35 +16,34 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    //    private DataSource dataSource;
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-//    @Autowired
+    //    @Autowired
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-        /*
-        * 다형성 -> 인터페이스를 두고 구현체만 바꾸어 적용하면 된다.
-        * SOLID 원칙 - OCP(Open-Closed Principle, 개방 폐쇄 원칙)
-            - 확장에는 열려 있고, 수정 변경에는 닫혀있다.
-        * 스프링의 DI (Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고, 설정만으로 구현클래스를 변경할 수 있다.
-         */
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//        /*
+//        * 다형성 -> 인터페이스를 두고 구현체만 바꾸어 적용하면 된다.
+//        * SOLID 원칙 - OCP(Open-Closed Principle, 개방 폐쇄 원칙)
+//            - 확장에는 열려 있고, 수정 변경에는 닫혀있다.
+//        * 스프링의 DI (Dependencies Injection)을 사용하면 기존 코드를 전혀 손대지 않고, 설정만으로 구현클래스를 변경할 수 있다.
+//         */
+//    }
 }
